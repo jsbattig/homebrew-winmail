2b231d0e309c2e56eddc0dd0bbfe5c1df6afd8f6
Winmail Opener <example@example.com>
2025-05-06 17:29:16 -0500

    Fix syntax error in py-winmail-opener formula test block

--- py-winmail-opener.rb
+++ py-winmail-opener.rb
@@ -1,8 +1,8 @@
 class PyWinmailOpener < Formula
   desc "Extract attachments and email body from Winmail.dat files"
   homepage "https://github.com/jsbattig/py-winmail-opener"
-  url "https://github.com/jsbattig/py-winmail-opener/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  url "https://github.com/jsbattig/py-winmail-opener/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "1fbd0ef6e6dd9672a7dac2939670c409079ebd4c3c9acfdd5f4ae382fafccabe"
   license "MIT"
 
   depends_on "python@3.10"
@@ -11,16 +11,16 @@ class PyWinmailOpener < Formula
   def install
     # Install Python files to libexec to avoid conflicts
     libexec.install Dir["*"]
-    
+
     # Create bin directory if it doesn't exist
     bin.mkpath
-    
+
     # Create a wrapper script in bin
     (bin/"winmail-opener").write <<~EOS
       #!/bin/bash
       "/usr/local/bin/python3.10" "#{libexec}/winmail_opener.py" "$@"
     EOS
-    
+
     # Make the wrapper executable
     chmod 0755, bin/"winmail-opener"
   end
@@ -34,6 +34,6 @@ class PyWinmailOpener < Formula
 
   test do
     # Test the version output
-    assert_match "winmail_opener 1.0.1")
+    assert_match "winmail_opener 1.0.0", shell_output("#{bin}/winmail-opener --version")
   end
 end
